3da5f6ba3ef6b7e82a177f22916c92dd1607e630
Kostiantyn Pavlenko <kostianpv@gmail.com>
2025-02-18 22:32:13 +0200

    added semicolon after alert

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ let name = prompt("Enter your name");
 let age = prompt("Enter your age");
 let sex = prompt("Enter your gender");
 
-alert(`Person information\nName: ${name}\nAge: ${age}\nSex: ${sex}`)
+alert(`Person information\nName: ${name}\nAge: ${age}\nSex: ${sex}`);
