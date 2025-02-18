9ffac3ae1455a80808a809350456309e28f31550
Kostiantyn Pavlenko <kostianpv@gmail.com>
2025-02-18 22:28:44 +0200

    practice with template strings

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt("Enter your name");
+let age = prompt("Enter your age");
+let sex = prompt("Enter your gender");
+
+alert(`Person information\nName: ${name}\nAge: ${age}\nSex: ${sex}`)
