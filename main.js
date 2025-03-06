e2606c2adede9e4fb8d3149edf22e5eaa2bea27f
Kostiantyn Pavlenko <kostianpv@gmail.com>
2025-03-06 13:54:24 +0200

    refactor solution

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 function guessNumber() {
-  const attempts = 10;
+  const attempts = 3;
 
   for (let i = 1; i <= attempts; i++) {
     const enteredValue = +prompt('Enter number greater than 100');
     const isEnteredValueNaN = Number.isNaN(enteredValue);
 
     if (!isEnteredValueNaN) {
-      if (enteredValue < 100) {
-        if(i === attempts){
-          alert('You entered less than 100.\nIt was your last attempt');
-        } else {
-          alert('You entered less than 100.\nPlease try again');
-        }
-        
-        continue;
-      }else {
+      if(enteredValue >= 100) {
         alert('You entered valid number');
         
         return enteredValue;
+      }
+      if(i === attempts){
+        alert('You entered less than 100.\nIt was your last attempt');
+      } else {
+        alert('You entered less than 100.\nPlease try again');
       }
     } else {
       if(i === attempts){
